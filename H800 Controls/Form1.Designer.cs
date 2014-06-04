6cfa5e591f9e96ada86e1dde2b496aca075e66cf
Piepieonline <piepieonline@gmail.com>
2014-06-04 12:23:27 +1000

    First Commit - Working on the active window

---
+++ H800 Controls/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace H800_Controls
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.currentAssignmentText = new System.Windows.Forms.TextBox();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyMenu = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.statusMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.openStatusWindowToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsModeChk = new System.Windows.Forms.CheckBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pressedLabel = new System.Windows.Forms.Label();
+            this.notifyMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.AutoSize = true;
+            this.statusLabel.Location = new System.Drawing.Point(119, 9);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(35, 13);
+            this.statusLabel.TabIndex = 0;
+            this.statusLabel.Text = "label1";
+            // 
+            // currentAssignmentText
+            // 
+            this.currentAssignmentText.Location = new System.Drawing.Point(156, 139);
+            this.currentAssignmentText.Name = "currentAssignmentText";
+            this.currentAssignmentText.Size = new System.Drawing.Size(100, 20);
+            this.currentAssignmentText.TabIndex = 2;
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.ContextMenuStrip = this.notifyMenu;
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "Logitech H800 Controller";
+            this.trayIcon.Visible = true;
+            this.trayIcon.DoubleClick += new System.EventHandler(this.notifyIcon1_DoubleClick);
+            // 
+            // notifyMenu
+            // 
+            this.notifyMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.statusMenuItem,
+            this.toolStripSeparator1,
+            this.openStatusWindowToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.notifyMenu.Name = "notifyMenu";
+            this.notifyMenu.Size = new System.Drawing.Size(147, 76);
+            // 
+            // statusMenuItem
+            // 
+            this.statusMenuItem.Name = "statusMenuItem";
+            this.statusMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.statusMenuItem.Text = "Disconnected";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(143, 6);
+            // 
+            // openStatusWindowToolStripMenuItem
+            // 
+            this.openStatusWindowToolStripMenuItem.Name = "openStatusWindowToolStripMenuItem";
+            this.openStatusWindowToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.openStatusWindowToolStripMenuItem.Text = "Settings";
+            this.openStatusWindowToolStripMenuItem.Click += new System.EventHandler(this.openStatusWindowToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(146, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingsModeChk
+            // 
+            this.settingsModeChk.Appearance = System.Windows.Forms.Appearance.Button;
+            this.settingsModeChk.AutoSize = true;
+            this.settingsModeChk.Location = new System.Drawing.Point(70, 42);
+            this.settingsModeChk.Name = "settingsModeChk";
+            this.settingsModeChk.Size = new System.Drawing.Size(137, 23);
+            this.settingsModeChk.TabIndex = 3;
+            this.settingsModeChk.Text = "Change Key Assignments";
+            this.settingsModeChk.UseVisualStyleBackColor = true;
+            this.settingsModeChk.CheckedChanged += new System.EventHandler(this.settingsModeChk_CheckedChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 142);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(72, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Pressed Key: ";
+            // 
+            // pressedLabel
+            // 
+            this.pressedLabel.AutoSize = true;
+            this.pressedLabel.Location = new System.Drawing.Point(90, 142);
+            this.pressedLabel.Name = "pressedLabel";
+            this.pressedLabel.Size = new System.Drawing.Size(0, 13);
+            this.pressedLabel.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.pressedLabel);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.settingsModeChk);
+            this.Controls.Add(this.currentAssignmentText);
+            this.Controls.Add(this.statusLabel);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.SizeGripStyle = System.Windows.Forms.SizeGripStyle.Hide;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Logitech H800 Controller";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.notifyMenu.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.TextBox currentAssignmentText;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+        private System.Windows.Forms.ContextMenuStrip notifyMenu;
+        private System.Windows.Forms.ToolStripMenuItem statusMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem openStatusWindowToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.CheckBox settingsModeChk;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label pressedLabel;
+    }
+}
+
---
+++ H800 Controls/Form1.cs
@@ -0,0 +1,147 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using USBHIDDRIVER;
+
+using System.Runtime.InteropServices;
+
+namespace H800_Controls
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("user32.dll", SetLastError = true)]
+        static extern void keybd_event(byte bVk, byte bScan, int dwFlags, int dwExtraInfo);
+        public const int KEYEVENTF_EXTENDEDKEY = 0x0001;    //Key down flag
+        public const int KEYEVENTF_KEYUP = 0x0002;          //Key up flag
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Logitech H800
+        private USBHIDDRIVER.USBInterface usb = new USBInterface("vid_046d", "pid_0a29");
+
+        //Key Dict
+        private Dictionary<string, byte> keyMapping = new Dictionary<string, byte>();
+
+        //Settings control
+        private bool settingsMode;
+
+        /* Known Keys:
+         *      Song Down: 120
+         *      Song Up: 140
+         *      Play: 110
+         *      Volume Up: 101
+         *      Volume Down: 102
+         */
+
+        String usbOut= "";
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            keyMapping["101"] = 0xAF;//"VOLUME_UP"
+            keyMapping["102"] = 0xAE;//"VOLUME_DOWN"
+            keyMapping["110"] = 0xB3;//"MEDIA_PLAY_PAUSE"
+            keyMapping["120"] = 0xB0;//"MEDIA_NEXT"
+            keyMapping["140"] = 0xB1;//"MEDIA_PREVIOUS"
+            
+            if (usb.Connect())
+                updateConnectionStatus(true);
+            else
+                updateConnectionStatus(false);
+
+            usb.write( new byte[] { 0 } );
+
+            usb.enableUsbBufferEvent(new System.EventHandler(headsetEventCatcher));
+            usb.startRead();
+
+            this.Show();
+
+            while( true )
+            {
+                Application.DoEvents();
+                if (usbOut != "" && usbOut.Length == 3)
+                {
+                    if (keyMapping.ContainsKey(usbOut))
+                    {
+                        if (settingsMode)
+                        {
+                            //pressedLabel.Text = usbOut;
+                            currentAssignmentText.Text = keyMapping[usbOut].ToString();
+                        }
+                        else
+                        {
+                            //SendKeys.Send("{" + keyMapping[usbOut] + "}");
+                            keybd_event(keyMapping[usbOut], 0, KEYEVENTF_EXTENDEDKEY, 0);
+                            keybd_event(keyMapping[usbOut], 0, KEYEVENTF_KEYUP, 0);
+                        }
+                    }
+                    usbOut = "";
+                }
+            }
+        }
+
+        private void headsetEventCatcher(object sender, EventArgs e)
+        {
+            USBHIDDRIVER.List.ListWithEvent a = USBHIDDRIVER.USBInterface.usbBuffer;
+
+            byte[] eve = (byte[])USBHIDDRIVER.USBInterface.usbBuffer[0];
+            USBHIDDRIVER.USBInterface.usbBuffer.RemoveAt(0);
+
+            usbOut = "";
+            foreach( byte v in eve )
+                usbOut += v;
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            e.Cancel = true;
+
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void notifyIcon1_DoubleClick(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void openStatusWindowToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            trayIcon.Dispose();
+            Environment.Exit(0);
+        }
+
+        private void updateConnectionStatus( bool connected )
+        {
+            String outText = "";
+            if (connected)
+                outText = "Connected";
+            else
+                outText = "Disconnected";
+
+            statusLabel.Text = outText;
+            statusMenuItem.Text = outText;
+        }
+
+        private void settingsModeChk_CheckedChanged(object sender, EventArgs e)
+        {
+            settingsMode = settingsModeChk.Checked;
+        }
+    }
+}
